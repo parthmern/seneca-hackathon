03d4e212cd2f75f3a9b4e86fe7449291b4740d8e
parthmern <parthmern@gmail.com>
2024-02-25 16:40:43 -0500

    login page css added

--- frontend/src/pages/LoginForm.js
+++ frontend/src/pages/LoginForm.js
@@ -4,6 +4,8 @@ import { AppContext } from '../context/AppContext';
 import { useNavigate } from 'react-router-dom';
 import { UserDetailsId, dbId } from '../utils/environmentVars';
 import { Query } from 'appwrite';
+import { Input } from '../components/ui/input';
+import { Button } from '../components/ui/button';
 
 function LoginForm() {
   const [email, setEmail] = useState('');
@@ -64,30 +66,33 @@ function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
+    <form className='flex flex-col gap-y-5 w-[50%] mx-auto mt-10 justify-between items-center' onSubmit={handleSubmit}>
+      <label className='flex items-center gap-5 justify-evenly w-full'>
+        <p  className='text-white flex  font-bold '>Email</p>
+        <Input
           type="email"
           value={email}
           onChange={handleEmailChange}
           placeholder="Enter your email"
           required
+          className='text-white'
         />
       </label>
       <br />
-      <label>
-        Password:
-        <input
+
+      <label className='flex items-center gap-5 gap-x-5 justify-evenly w-full' >
+        <p className='text-white   font-bold '> Password </p>
+        <Input
           type="password"
           value={password}
           onChange={handlePasswordChange}
           placeholder="Enter your password"
           required
+          className='text-white'
         />
       </label>
       <br />
-      <button type="submit">Login</button>
+      <Button type="submit">Login</Button>
     </form>
   );
 }
